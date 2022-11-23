9d102e689d7fd22151f75235d70471d0a6da7332
Greiciane Oliveira <116230449+Greicianne@users.noreply.github.com>
2022-11-23 14:56:47 +0000

    Meus primeiros arquivos

---
+++ main.py
@@ -0,0 +1,20 @@
+#meu primeiro projeto python!!
+#
+
+#print() = comando de saida
+print("alo mundo")
+# quando quiser guardar uma string (frase)
+nome = "marcela"
+#quando quiser guardar um numero inteiro
+idade = 23
+#exibir o meu nome (que esta dentro da variável nome)
+print(nome)
+
+#quando quiser exibir a frase "minha idade é"complementado com o conteúdo da variável idade
+#print ("meu nome é " +nome)
+print("minha idade é:" + str(idade))
+print(f"minha idade é: {idade}")
+print("minha idade é: {}".format(idade))
+
+#quando quiser exibir "meu nome é... e tenho ...anos..." trocando pelas variáveis nome e idade
+print("meu nome é {} e tenho {} anos".format(nome, idade))
